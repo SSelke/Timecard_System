5f791ecf50f7e7897668e3dd1b78911d39a024f6
Scott <pravaca22@gmail.com>
2018-07-01 01:06:22 -0700

    Adding Date functionality

---
+++ public/assets/javascript/dashboard.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    var date = new Date();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    $("#calendar-month-year").html(monthNames[month] + " " + year);
+});
+
+console.log("hello")
--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -36,3 +36,21 @@ $("#delete-message").click(function(){
 //         });
 //     }
 // });
+
+// var days = getDaysArray(2018, 7)
+
+// console.log(days);
+
+
+
+// function getDaysArray(year, month) {
+//     const names = Object.freeze(
+//         ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']);
+//     const date = new Date(year, month - 1, 1);
+//     const result = [];
+//     while (date.getMonth() == month - 1) {
+//         result.push(`${date.getDate()}-${names[date.getDay()]}`);
+//         date.setDate(date.getDate() + 1);
+//     }
+//     return result;
+// }
